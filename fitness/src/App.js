42cd4dbe8c31cc2598fb7816d338a922a8865fc4
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-03-15 17:39:02 -0400

    Updated

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import { Route } from "react-router-dom"
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import { useState,useEffect } from 'react'
+import { URL, config1 } from "./services"
+import ShowWorkouts from "./components/ShowWorkouts"
+import Form from "./components/Form"
+
 
 function App() {
+  const [workout, setWorkout] = useState([]);
+  useEffect(() => {
+    const makemyworkout = async () => {
+      let resp = await axios.get(URL,config1);
+      console.log(resp.data.records)
+      setWorkout(resp.data.records)
+    };makemyworkout()
+  },[])
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Route exact path="/">
+        Home Page!
+      </Route>
+      <Route path="/userWorkout">
+        {workout.map((val) => (
+          <ShowWorkouts workout={val}/>
+        ) )}
+      </Route>
+      <Route path="/addWorkout">
+       <Form />
+      </Route>
+      <footer> <Footer/></footer>
+      console.log("testing")
     </div>
+    
   );
 }
 
