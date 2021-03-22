831b5ffd835d2c3173c1d1eb221c78e1110d7bc0
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-03-22 11:36:15 -0400

    update

--- fitness/src/components/Nav.jsx
+++ fitness/src/components/Nav.jsx
@@ -1,33 +1,35 @@
 import { Link } from "react-router-dom";
-import { useHistory } from 'react-router-dom';
-const Nav = () => {
-const history=useHistory()
+import { useHistory } from "react-router-dom";
+function Nav(){
+  //const history = useHistory();
   const push = () => {
-    history.push("/nutrition")
+    //history.push("/nutrition");
     //window.location.reload();
-  }
+  };
   return (
     <nav id="nav">
-      
-      
-        <Link id="Home" to="/">
-          HomePage
-        </Link>
-        <Link id="Home" to="/userWorkout">
-          Public Workout Routines
-        </Link>
-        <Link id="Home" to="/addWorkout">
-          Add Routine
-        </Link>
-        <Link id="Home" to="/signUp">
-          Sign Up
-        </Link>
-        <div id="fitnesspal2">
-        <button id="fitbtn" onClick={push}><img id="fitnesspal" src="https://i.pcmag.com/imagery/reviews/065uaiCApaX7sVvWBH2jmtJ-3..1576546458.jpg" /></button>
-        </div>
-        <h1>My Fitness Journey</h1>
-      </nav>
-    
+      <Link id="Home" to="/">
+        HomePage
+      </Link>
+      <Link id="Home" to="/userWorkout">
+        Public Workout Routines
+      </Link>
+      <Link id="Home" to="/addWorkout">
+        Add Routine
+      </Link>
+      <Link id="Home" to="/signUp">
+        Sign Up
+      </Link>
+      <div id="fitnesspal2">
+        <button id="fitbtn" onClick={push}>
+          <img
+            id="fitnesspal"
+            src="https://i.pcmag.com/imagery/reviews/065uaiCApaX7sVvWBH2jmtJ-3..1576546458.jpg"
+          />
+        </button>
+      </div>
+      <h1>My Fitness Journey</h1>
+    </nav>
   );
 };
 
