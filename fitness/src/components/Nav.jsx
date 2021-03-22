492ca3cf0ce9a747d3ff87aae54f281ba5cd33f5
Andri Rafti <andrirafti@Andris-MacBook-Air.local>
2021-03-22 11:43:45 -0400

    update

--- fitness/src/components/Nav.jsx
+++ fitness/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 function Nav(){
   //const history = useHistory();
   const push = () => {
